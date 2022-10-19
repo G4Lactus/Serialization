44cc07c27cf70e171755cc506f3dc8b2090c2016
G4Lactus <F-Scheidt@gmx.de>
2022-10-19 13:17:08 +0200

    update walkthrough cereal

--- SER_06_Serialization_Functions_3.cpp
+++ SER_06_Serialization_Functions_3.cpp
@@ -1,5 +1,8 @@
 // Explicit versioning and Inheritance
 // ----------------------------------------
+
+// Explicit versioning
+// ------------------------------------
 // cereal supports explicit versioning for types, much like Boost class 
 // versioning.
 // In cereal it is optional, the parameter has always to be a 
@@ -22,7 +25,7 @@
 // 
 
 #include <iostream>
-// [[Rcpp::depeneds(Rcereal)]]
+// [[Rcpp::depends(Rcereal)]]
 #include <cereal/cereal.hpp>
 #include <Rcpp.h>
 
@@ -36,7 +39,8 @@ struct MyCoolClass
     // You can choose different behaviors depending on the version
     // This is useful if you need to support older variants of your codebase
     // interacting with newer ones
-    if (version > some_number) {
+    std::size_t some_version{42};
+    if (version > some_version) {
       Rcpp::Rcout << "Your version is more recent!" << std::endl;
     } else {
       Rcpp::Rcout << "Your version is older or equal!"<< std::endl;
@@ -58,10 +62,74 @@ template<class Archive>
 void load(Archive& ar, AnotherType& at, std::uint32_t const version) {}
 
 // Associate some type with a version number
+CEREAL_CLASS_VERSION(MyCoolClass, 42);
 
-
-// iIf we don't associate a class with a version number and use a versioned
+// If we don't associate a class with a version number and use a versioned
 // serialize function, its version number will default to 0.
 
 
 
+// Inheritance
+// ------------------------------------
+// Serialization functions, like any other function, will be inherited by
+// derived classes.
+#include <cereal/archives/binary.hpp>
+
+struct MyBase
+{
+  template<class Archive>
+  void serialize(Archive&) {}
+  
+};
+
+
+struct MyDerived: MyBase
+{
+  template<class Archive>
+  void load(Archive& ) const {}
+  
+  template<class Archive>
+  void save(Archive& ) const {}
+};
+
+
+// [[Rcpp::export]]
+int main()
+{
+  MyDerived d;
+  cereal::BinaryOutputArchive ar(std::cout);
+  ar(d);
+  
+  return 0;
+}
+
+// potentiall here a static assertion failure could arise during compile time,
+// because cereal detects both: a serialize and save/load pair for MyDerived.
+// MyDerived inhertis the public serialize from MyBase, thus giving it both a
+// serialize and load-save pair, a disambiguate situation for cereal.
+
+
+// Even if serialize was made private in the base class, cereal may still have
+// access to it from the derived class because of a friend to cereal::access,
+// resulting in the same error.
+#include <cereal/access.hpp>
+namespace cereal 
+{
+  template<class Archive>
+  struct specialize<Archive, MyDerived, cereal::specialization::member_load_save>{};
+  // cereal no longer has any ambiguity when serializing MyDerived
+}
+
+// CEREAL_SPECIALIZE_FOR_ALL_ARCHIVES(MyDerived, cereal::specialization::member_load_save)
+
+// Default construction
+// cereal requires access to a default constructor for types it serializes.
+// If you don’t want to provide a default constructor but do want to serialize
+// smart pointers to it, you can get around this restriction using a special
+// overload, detailed in the pointers section of the documentation.
+
+// Renaming serialization functions
+// If your coding conventions conflict with the expected names for cereal
+// serialization functions or if they conflict for any other reason, you can use
+// the macros in <cereal/macros.hpp> to change these names.
+// See the doxygen documentation for more information.
---
+++ SER_08_Inheritance.cpp
@@ -0,0 +1,93 @@
+// Inheritance
+// --------------------------
+// cereal fully supports serializing class inheritance and serialization.
+
+
+// Normal Inheritance
+// -----------------------
+// [[Rcpp::depends(Rcereal)]]
+#include <cereal/types/base_class.hpp>
+#include <Rcpp.h>
+
+struct Base
+{
+  int x;
+  
+  template<class Archive>
+  void serialize(Archive& ar) { ar(x); }
+  
+};
+
+
+struct Derived : public Base
+{
+  int y;
+  template<class Archive>
+  void serialize(Archive & ar)
+  {
+    // We pass this cast to the base type for each base type we need to 
+    // serialize. Do this insteead of calling serialize functions directly.
+    ar(cereal::base_class<Base>(this), y);
+  }
+  
+};
+
+
+// Virtual Inheritance
+// -----------------------
+// Only one copy of each base class will be contained within a derived object.
+// cereal provides a method for tracking virtual base classes, preventing 
+// multiple copies from being serialized.
+// If cereal::base_class were used in a situation where the same base class
+// appeared in more than on traversal of the class hierarchy, it could
+// potentially result in duplicate information being serialized.
+// USE: cereal::virtual_base_class, to track the instantiation of base objs
+// for a derived class, ensuring that only one copy of each base class is
+// serialized.
+#include <cereal/types/base_class.hpp>
+
+struct BaseA
+{
+  int x;
+  // Note the non-member serialize - trying to call serialize from a derived
+  // class wouldn't work
+};
+
+
+template<class Archive>
+void serialize(Archive& ar, BaseA& b) {ar(b.x);}
+
+struct Left : virtual BaseA
+{
+  int l;
+  template<class Archive>
+  void serialize(Archive& ar) {ar(cereal::virtual_base_class<BaseA>(this), l);}
+};
+
+
+struct Right : virtual BaseA
+{
+  int r;
+  template<class Archive>
+  void serialize(Archive& ar) {ar(cereal::virtual_base_class<BaseA>(this), r);}
+};
+
+
+struct DerivedA : virtual Left, virtual Right
+{
+  int y;
+  template<class Archive>
+  void serialize(Archive& ar) {ar(cereal::virtual_base_class<Left>(this),
+                                  cereal::virtual_base_class<Right>(this),
+                                  y);}
+};
+
+// With virtual inheritance and cereal::virtual_base_class, x, l, r, and y
+// will serialized exactly once.
+// If we had not used virtual_base_class, two copies of the base class may have
+// been serialized resulting in duplicate x entries.
+
+
+
+
+
