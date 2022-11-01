89942d77c4f630910304d916eccb8e2e75517840
G4Lactus <F-Scheidt@gmx.de>
2022-11-01 14:57:25 +0100

    Update introduce arma serialization

--- SER_03_cereal_STL_support_3.cpp
+++ SER_03_cereal_STL_support_3.cpp
@@ -1,3 +1,4 @@
+// [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::depends(Rcereal)]]
 #include <vector>
 #include <fstream>
@@ -6,7 +7,80 @@
 #include <cereal/archives/binary.hpp>
 #include <cereal/types/vector.hpp>
 // -----------------------------------
-#include <Rcpp.h>
+#include <RcppArmadillo.h>
+
+
+class Serialize_ArmaVec
+{
+private:
+  std::size_t nrows;
+  std::vector<double> vector_data;
+  
+  friend class cereal::access;
+  
+  template<class Archive>
+  void serialize(Archive& ar) {
+    ar(
+      CEREAL_NVP(nrows),
+      CEREAL_NVP(vector_data)
+    );
+  }
+};
+
+
+class Serialize_ArmaMat
+{
+private:
+  std::size_t nrows;
+  std::size_t ncols;
+  std::vector<double> matrix_data;
+  
+  friend class cereal::access;
+  
+  template<class Archive>
+  void serialize(Archive& ar) {
+    ar(
+      CEREAL_NVP(nrows),
+      CEREAL_NVP(ncols),
+      CEREAL_NVP(matrix_data)
+    );
+  }
+  
+public:
+  Serialize_ArmaMat(){};
+  Serialize_ArmaMat(arma::mat x)
+    : nrows{x.n_rows}, ncols{x.n_cols},
+      matrix_data{Rcpp::as<std::vector<double>(x.)}
+      {}
+  ~Serialize_ArmaMat() = default;
+  
+  Rcpp::NumericVector numMatrix() {
+    Rcpp::NumericVector d = Rcpp::wrap(matrix_data);
+    d.attr("dim") = Rcpp::Dimension(nrows, ncols);
+    return d;
+  }
+  
+  int get_nrows() const {
+    return nrows;
+  }
+  
+  int get_ncols() const {
+    return ncols;
+  }
+  
+  std::vector<std::size_t> get_dims() const {
+    std::vector<std::size_t> dims;
+    dims.push_back(get_nrows());
+    dims.push_back(get_ncols());
+    return dims;
+  }
+  
+  std::vector<double> get_matrix_data() const {
+    return matrix_data;
+  }
+
+};
+
 
 
 class Serialize_NumericMatrix
@@ -27,7 +101,6 @@ private:
     );
   }
   
-
 public:
   Serialize_NumericMatrix(){};
   Serialize_NumericMatrix(Rcpp::NumericMatrix x)
@@ -64,11 +137,13 @@ public:
 };
 
 
+
 class Fan_of_linear_Agebra
 {
 private:
   int i1, i2;
   Rcpp::NumericMatrix m;
+  arma::mat am;
 
   friend class cereal::access;
   
@@ -76,13 +151,15 @@ private:
   void save(Archive& archive) const
   {
     Serialize_NumericMatrix sNM{m};
+    Serialize_ArmaMat sAM{am};
     archive(
       CEREAL_NVP(i1),
       CEREAL_NVP(i2)
     );
     
     archive(
-      CEREAL_NVP(sNM)
+      CEREAL_NVP(sNM),
+      CEREAL_NVP(sAM)
     );
     
   }
@@ -97,7 +174,8 @@ private:
     );
     
     archive(
-      CEREAL_NVP(sNM)
+      CEREAL_NVP(sNM),
+      CEREAL_NVP(sAM)
     );
     m = reconstruct_numericMatrix(sNM);
   }
@@ -105,8 +183,8 @@ private:
   
 public:
   Fan_of_linear_Agebra() = default;
-  Fan_of_linear_Agebra(int i1, int i2, Rcpp::NumericMatrix m)
-    : i1{i1}, i2{i2}, m{m} {
+  Fan_of_linear_Agebra(int i1, int i2, Rcpp::NumericMatrix m, arma::mat am)
+    : i1{i1}, i2{i2}, m{m}, am{am} {
     };
   ~Fan_of_linear_Agebra() = default;
   
@@ -120,7 +198,11 @@ public:
   
   Rcpp::NumericMatrix get_NumericMatrix() const {
     return m;
-  } 
+  }
+  
+  arma::mat get_ArmaMat() const {
+    return am;
+  }
   
   Rcpp::NumericMatrix reconstruct_numericMatrix(Serialize_NumericMatrix sNM) {
     std::size_t mat_size = sNM.get_ncols()*sNM.get_nrows();
@@ -132,6 +214,11 @@ public:
     return mat;
   }
   
+  arma::mat reconstruct_armaMat(Serialize_ArmaMat sAM) {
+    std::size_t mat_size;
+
+  }
+  
 };
 
 
@@ -140,7 +227,10 @@ void serialize() {
   cereal::BinaryOutputArchive oarchive(os);
   Rcpp::NumericMatrix mat(5, 5);
   std::fill(mat.begin(), mat.end(), 42);
-  Fan_of_linear_Agebra fola(4, 42, mat);
+  arma::mat am(4, 8);
+  am.fill(42);
+  
+  Fan_of_linear_Agebra fola(4, 42, mat, am);
   Rcpp::Rcout << fola.get_NumericMatrix() << std::endl;
   oarchive(cereal::make_nvp("Fan of linear algebra: ", fola));
   
