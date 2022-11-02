4d7f4a0b86f6a20cc53b9a096f7d5ce8b75050a2
G4Lactus <F-Scheidt@gmx.de>
2022-11-02 15:42:09 +0100

    Added example with sp_mat only.

---
+++ SER_04_Serialize_Arma_with_Binary_2.cpp
@@ -0,0 +1,70 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(Rcereal)]]
+#include <iostream>
+#include <fstream>
+#include <cereal/archives/binary.hpp>
+#include <cereal/access.hpp>
+#include <RcppArmadillo.h>
+
+
+namespace arma {
+
+  template<class Archive>
+  void save(Archive & ar, const arma::sp_mat &t){
+    ar(t.n_rows, t.n_cols, t.n_nonzero);
+    
+    for (auto it = t.begin(); it != t.end(); ++it) {
+      ar(it.row(), it.col(), *it);
+    }
+  }
+
+  template<class Archive>
+  void load(Archive & ar, arma::sp_mat &t) {
+    arma::uword r, c, nz;
+    ar(r, c, nz);
+    
+    t.zeros(r, c);
+    while (nz--) {
+      double v;
+      ar(--r, --c, v);
+      t(r, c) = v;
+    }
+  }
+}
+
+
+
+// [[Rcpp::export]]
+int main() {
+  
+  { // Serialize
+    arma::mat C(3, 3, arma::fill::randu);
+    C(0, 0) = 0;
+    C(1, 1) = 0; // example so that a few of the components are u
+    C(1, 2) = 0;
+    C(2, 0) = 0;
+    C(2, 1) = 0;
+    arma::sp_mat const spA = arma::sp_mat(C);
+    assert(spA.n_nonzero == 4);
+    spA.print("spA: ");
+    
+
+    std::ofstream os("Backend/Serialize_Arma.bin", std::ios::binary);
+    cereal::BinaryOutputArchive oarchive(os);
+    oarchive(spA);
+  }
+  
+  // .... put put put ... 
+  
+  { // Deserialize
+    arma::sp_mat spB;
+    
+    std::ifstream is("Backend/Serialize_Arma.bin", std::ios::binary);
+    cereal::BinaryInputArchive iarchive(is);
+    iarchive(spB);
+    
+    spB.print();
+  }
+  
+  return 0;
+}
